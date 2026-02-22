20a2c637abb779763e8f2ad02254967391fdb7b3
Delemangi <milev.stefan@gmail.com>
2026-02-22 18:09:34 +0100

    Fix import path for RetrievalError exception handling

--- api/app/main.py
+++ api/app/main.py
@@ -13,7 +13,7 @@ from app.api.health import router as health_router
 from app.api.links import router as links_router
 from app.api.questions import router as questions_router
 from app.data.connection import Database
-from app.llms.context import RetrievalError
+from app.utils.exceptions import RetrievalError
 from app.utils.http_client import close_http_client, init_http_client
 from app.utils.logger import setup_logging
 from app.utils.settings import Settings
